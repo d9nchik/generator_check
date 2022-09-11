06ee80b7f97b865ed9fd9e1e2f57323300c151bc
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-09-11 14:14:56 +0300

    add NormalGenerator visualization

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-from math import log
-
 from src.analyzer import Analyzer
-from src.generator import EqualDistributionGenerator
+from src.generator import NormalGenerator
 
 if __name__ == "__main__":
-    a_array = [5 ** 13, 5 ** 11, 5 ** 9, 5 ** 5, 5 ** 1]
-    c_array = [2 ** 31, 2 ** 21, 2 ** 15, 2 ** 7, 2 ** 3]
+    a_array = [0.01, 0.1, 1, 10, 100]
+    sigma_array = [0.01, 0.1, 1, 10, 100]
 
-    a_c_array_combinations = [(a, c) for a in a_array for c in c_array]
+    a_sigma_array_combinations = [(a, sigma) for a in a_array for sigma in sigma_array]
 
-    for a, c in a_c_array_combinations:
-        generator = EqualDistributionGenerator(a, c)
+    for a, sigma in a_sigma_array_combinations:
+        generator = NormalGenerator(a, sigma)
 
         analyzer = Analyzer(generator)
+        analyzer.show_frequency_histogram()
         chi_square, critical_chi_square = analyzer.get_chi_square()
         row = '\t'.join(map(lambda x: str(round(x, 3)).replace('.', ','),
-                            [log(a, 5), log(c, 2), analyzer.mean(), analyzer.dispersion(), chi_square,
+                            [a, sigma, analyzer.mean(), analyzer.dispersion(), chi_square,
                              critical_chi_square]))
         print(row)
